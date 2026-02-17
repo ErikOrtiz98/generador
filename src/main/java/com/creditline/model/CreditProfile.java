4a77c021705d34748ab747e655debd1fe925c0c3
ErikOrtiz98 <92230759+ErikOrtiz98@users.noreply.github.com>
2026-02-17 13:56:30 -0600

    Create CreditProfile.java file

---
+++ src/main/java/com/creditline/model/CreditProfile.java
@@ -0,0 +1,12 @@
+package com.creditline.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreditProfile {
+    private double creditLine;
+}
