12ec2c8379246da8199acf6d7d73405a2f7bd354
ErikOrtiz98 <92230759+ErikOrtiz98@users.noreply.github.com>
2026-02-17 13:55:12 -0600

    Create ApiResponse.java file

---
+++ src/main/java/com/creditline/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.creditline.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+    private ResponseMeta meta;
+    private ResponseData data;
+}
